72c013527acbe8af88ccfdec2d6662e70a74a15e
ACT-STUDENT <ACT-STUDENT@LAB1-PC48>
2026-07-07 10:55:22 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ninel_INFASS2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
